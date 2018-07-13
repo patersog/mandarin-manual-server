2335ad1bb533b93cf66d08007458ed3c9bc103c1
patersog <gianluca.paterson@gmail.com>
2018-07-13 16:06:40 -0400

    Refactor /PUT questions endpoint & removed console.logs, fix pointer bug

--- routes/questions.js
+++ routes/questions.js
@@ -6,16 +6,6 @@ const Questions = require('../models/questions');
 
 const router = express.Router();
 
-// router.get('/', (req, res, next) => {
-// 	Questions.find()
-// 		.then(results => {
-// 			res.json(results);
-// 		})
-// 		.catch(err => {
-// 			next(err);
-// 		});
-// });
-
 router.get('/:username', (req, res, next) => {
 	const {username} = req.params;
 	Users.findOne({'username': username})
@@ -35,10 +25,8 @@ router.get('/:username', (req, res, next) => {
 });
 
 router.get('/correct/:username', (req, res, next) => {
-	console.log('WERE IN!');
 	const {answer} = req.query;
 	const {username} = req.params;
-	console.log(username, answer);
 	Users.findOne({'username': username})
 		.select('questions.head')
 		.then(result => {
@@ -68,51 +56,40 @@ router.put('/next/:username', (req,res, next) => {
 	let answeredIndex;
 	let nextQuestionIndex;
 
-	let user; //need reference to user for 'save'
+	let user;
 
 	Users.findOne({'username': username})
 		.then(result => {
 			user = result;
 			const {head, list} = user.questions;
 			nodeList = list;
-			return nodeList.filter((node) => {
+			return nodeList.filter((node, index) => {
 				if(node.qid.toString() === head.toString()) {
+					answeredIndex = index;
 					return node;
 				}
 			});
 		})
 		.then(node => {
 
-			// console.log('NODE', node[0]);
-			// update it's m value
 			let current = node[0];
 			answered = current;
 			nextQuestionIndex = answered.next;
 			answered.m = correct ? answered.m * 2 : 1;
 
-			//console.log('M_VALUE',typeof(answered.m));
-
 			// save it's m value to find correct placement
 			let m_position = answered.m;
 
-			console.log(m_position);
-			//console.log('NODE INSERT POSITION', m_position);
-
 			// find the insertion point for our 'node'
-			while(m_position >= 1 && current.next <= nodeList.length) {
+			while(m_position  && current.next <= nodeList.length) {
 				current = nodeList[current.next];
-				console.log('new current', current);
 				m_position--;
 			}
-
-			console.log('we be looped!');
 			// set the new head to be the next question qid
 			user.questions.head = nodeList[nextQuestionIndex].qid;
 
 			// update 'next' pointers for insertion
-			console.log('answered.next before', answered.next);
 			answered.next = current.next;
-			console.log('answered.next after:', answered.next);
 			current.next = answeredIndex;
 
 			/**
